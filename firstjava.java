b41200abe611a0f3af337526be7f684d55e202dd
Ramchandratemani <93098340+Ramchandratemani@users.noreply.github.com>
2023-06-10 20:41:39 +0530

    Add files via upload

---
+++ firstjava.java
@@ -0,0 +1,32 @@
+package firstsl;
+
+public class firstjava {
+	
+	
+public static void main(String[] args) {
+	int i;
+	int j=10;
+	int k=5;
+	for(i=1;i<=5;i++) {
+		System.out.println(i);
+	}
+while(j>=5) {
+	System.out.println(j);
+	j--;
+}
+
+do {
+	System.out.println(j+i);
+	k++;
+}
+while(k<=9);
+
+String str="India";
+
+for(int a=str.length()-1;a>=0;a--) {
+	System.out.print(str.charAt(a));
+}
+
+
+}
+}
